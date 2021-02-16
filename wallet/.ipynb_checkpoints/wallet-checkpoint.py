3644c28c3f86aa969ef6ab58de77e4e14155d428
kris-ai <kishkris@gmail.com>
2021-02-16 14:40:12 -0800

    updated

--- wallet/.ipynb_checkpoints/wallet-checkpoint.py
+++ wallet/.ipynb_checkpoints/wallet-checkpoint.py
@@ -3,13 +3,16 @@ import subprocess
 import json
 import os
 
-mnemonic = os.getenv('MNEMONIC', 'define sample clip alarm lazy bone doll vendor tell garage jealous dolphin')
+def derive_wallets(coin):
 
-command = f'./derive -g --mnemonic={mnemonic} --cols=path,address,privkey,pubkey --numderive=3 --format=json'
+    mnemonic = os.getenv('MNEMONIC', 'define sample clip alarm lazy bone doll vendor tell garage jealous dolphin')
+    
+    command = f'./derive -g --mnemonic={mnemonic} --coin={coin} --cols=path,address,privkey,pubkey --numderive=3 --format=json'
+    
+    p = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
+    output, err = p.communicate()
+    p_status = p.wait()
 
-p = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
-output, err = p.communicate()
-p_status = p.wait()
-
-keys = json.loads(output)
-print(keys)
+    keys = json.loads(output)
+    
+    return keys
